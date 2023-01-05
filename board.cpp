e4b1e412a1a4e7ae993e34c9c5782eb0cbd69d17
Arnav Sajith <sajia5142@outlook.com>
2023-01-05 18:59:24 -0500

    final commit of Jan 5th, added pawn class

--- board.cpp
+++ board.cpp
@@ -2,62 +2,74 @@
 #include <string>
 using namespace std;
 
-class Piece{
-private:
-  
 
-public:
-  string type;
-  bool color;
-  int x;
-  int y;
-  Piece(int x, int y, string type, bool color) : x(x), y(y), type(type), color(color) {}
+class Piece{
+    private:
+    public:
+    string type;
+    bool color;
+    int x;
+    int y;
+    int id;
+
+    
+    Piece(int x, int y, string type, bool color) : x(x), y(y), type(type), color(color) {}
+    void move(int new_x, int new_y) {
+        x = new_x;
+        y = new_y;
+    }
 };
 
+class Pawn : public Piece{
+    private:
+    public:
+    Pawn(int x, int y, bool color) : Piece(x, y, "pawn", color){}
+
+};
+
+
 class Board {
-private:
+    private:
+    public: 
+    Piece* board[8][8];
 
-public: 
-Piece* board[8][8];
-
-Board(){
-    for(int x = 0; x < 8; x++) {
-        for(int y = 0; y < 8; y++) {
-            board[x][y] = nullptr; // Initialize the element to a null pointer
-        }
-    }
-}
-void add_pieces(){
-    for(int x = 0; x < 8; x++) {
-        for(int y = 0; y < 8; y++) {
-            if (x == 1){
-                board[x][y] = new Piece(x, y, "pawn", 0);
-            }
-            else if (x == 6) {
-                board[x][y] = new Piece(x, y, "pawn", 1);
+    Board(){
+        for(int x = 0; x < 8; x++) {
+            for(int y = 0; y < 8; y++) {
+                board[x][y] = nullptr; // Initialize the element to a null pointer
             }
         }
     }
-}
-
-void show_board(){
-    for(int x = 0; x < 8; x++) {
-        for (int y = 0; y < 8; y++) {
-            if (board[x][y] != nullptr) { // Check whether the pointer is a null pointer
-                cout << board[x][y]->type << " ";
-            } else {
-                cout << "**** "; // Print a blank space if the pointer is a null pointer
+    void add_pieces(){
+        for(int x = 0; x < 8; x++) {
+            for(int y = 0; y < 8; y++) {
+                if (x == 1){
+                    board[x][y] = new Pawn(x, y, 0);
+                }
+                else if (x == 6) {
+                    board[x][y] = new Pawn(x, y, 1);
+                }
             }
         }
-        cout << endl;
     }
-}
+
+    void show_board(){
+        for(int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                if (board[x][y] != nullptr) { // Check whether the pointer is a null pointer
+                    cout << board[x][y]->type << " ";
+                } else {
+                    cout << "**** "; // Print a blank space if the pointer is a null pointer
+                }
+            }
+            cout << endl;
+        }
+    }
 
 };
 
 int main() {
     Board board1;
-
     board1.show_board();
     cout << endl << endl << endl;
     board1.add_pieces();
